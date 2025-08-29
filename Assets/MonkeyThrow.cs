929053b9b1406cb4931eec86d0e25c847711ea99
amirullahizzan@gmail.com <amirullahizzan@gmail.com>
2025-08-29 23:25:19 +0900

    [update] monkeythrow.cs

--- Assets/MonkeyThrow.cs
+++ Assets/MonkeyThrow.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class MonkeyThrow : MonoBehaviour
 {
+    [SerializeField] private GameObject thrownObjPrefab;
+     [SerializeField] private GameObject throwPositionObj;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        GameObject inst = Instantiate(thrownObjPrefab, throwPositionObj.transform.position, transform.rotation);
     }
 }
