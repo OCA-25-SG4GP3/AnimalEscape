61f1df480de5b4ab1db84532df169342cf0489ed
501B-13\cre <amir>
2025-09-29 19:58:18 +0900

    [add] 監視カメラ

---
+++ Assets/YUKIOKA/SurveillanceCamera/SearchPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchPlayer : MonoBehaviour
+{
+    public float angle = 45f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player") //視界の範囲内の当たり判定
+        {
+            Debug.Log("視界の範囲内");
+
+            //視界の角度内に収まっているか
+            Vector3 posDelta = other.transform.position - transform.position;
+            float playerAngle = Vector3.Angle(transform.forward, posDelta);
+
+            if (playerAngle < angle) //player_angleがangleに収まっているかどうか
+            {
+                Debug.Log("監視カメラがプレイヤーを発見しました！");
+                ////オブジェクトの色を赤に変更する
+                //other.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
