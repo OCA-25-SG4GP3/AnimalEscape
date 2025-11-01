10e6ba0f344084a4cf326387445ce2a7b84b126d
502-06\cre <m12955746@gmail.com>
2025-11-01 16:06:33 +0900

    スクリプトのほうには書いたけどまだつけたりはできていない

---
+++ Assets/PenguinAnimationEvent.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PenguinAnimationEvent : MonoBehaviour
+{
+    public AudioSource audioSource; // ‰¹Œ¹iInspector‚Åİ’èj
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound()
+    {
+
+        audioSource.Play(); // SE‚ğÄ¶
+    }
+}
