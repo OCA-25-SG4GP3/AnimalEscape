17654f753c362cb767e1639e0ddbd4cf9af3171f
EtoGP <fgnkshakspryAsg@gmail.com>
2025-12-13 17:32:43 +0900

    GaugeFlickering.csのコメントを一部修正しました

--- Assets/GaugeFlickering.cs
+++ Assets/GaugeFlickering.cs
@@ -3,14 +3,10 @@ using UnityEngine.UI;
 
 public class GaugeFlickering : MonoBehaviour
 {
-    [SerializeField]private Animator anim;
-    [SerializeField]private Slider zooKeeperSlide;
+    [SerializeField]private Animator anim;         //再生するアニメーションを取得してくる
+    [SerializeField]private Slider zooKeeperSlide; //参照するスライダーを取得してくる
 
-    private const float ONE_THIRD = 0.666666f;    
-
-    //Animator animator = GetComponent<Animator>();
-    //animator.Play("GaugeFlickerImage");
-
+    private const float ONE_THIRD = 0.666666f; //1/3を再現するための定数    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,8 +17,7 @@ public class GaugeFlickering : MonoBehaviour
     void Update()
     {
         //残りのゲージが三分の一以下なら
-        if(zooKeeperSlide.value >= zooKeeperSlide.maxValue* ONE_THIRD)
-        //if(zooKeeperSlide.value >= 100.0f)
+        if(zooKeeperSlide.value >= zooKeeperSlide.maxValue* ONE_THIRD)        
         {
             anim.Play("GaugeFlickerImage");
         }
