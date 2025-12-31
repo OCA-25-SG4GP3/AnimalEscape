3762b299293cbd367234ddbd1fffc6cb1ef9b215
HibikiOtoha <matsu-ryo410@ezweb.ne.jp>
2025-12-31 17:21:34 +0900

    fix フェードインの仕様変更(アニメーションからimageのalpha変更へ)

--- Assets/CallStartUI.cs
+++ Assets/CallStartUI.cs
@@ -2,56 +2,59 @@ using UnityEngine;
 
 public class GameStartUI : MonoBehaviour
 {
-    //�Q�[���I�[�o�[�A�N���A�Ɠ����悤�ɂς��Ƃ����Ăς��Ə����悤�ɂ���
+    //ゲームオーバー、クリアと同じように、少し止めてから処理
     [SerializeField] private Vector3 firstPosition = new Vector3(0.0f, 0.0f, 0.0f);
-    [SerializeField] private bool isApear = false;  //�������o�������邩
-    [SerializeField] private float uiAppearSeconds = 2.0f;//UI�̏o������    
-    [SerializeField] private float decelTimeSpeedNDeltaTime = 1.0f;//�o�����Ԃ��������鑬��
+    [SerializeField] private bool isApear = false;  //UI表示中かどうか
+    [SerializeField] private float uiAppearSeconds = 4.0f;//UIの表示時間 (秒)
+    [SerializeField] private float decelTimeSpeedNDeltaTime = 1.0f;//表示時間を減らす速度
 
     [SerializeField] private GameObject fadeObject;
 
     private void Awake()
     {
-        //�����ʒu�ݒ�
-        //Vector3 newPosition = transform.position;
-        //newPosition = new Vector3(-(Screen.width * 0.5f), Screen.height * 0.5f, 0.0f);
-        //transform.position = newPosition;
-        //isAppear = true;
-
-        //UI�������ʒu�ɐݒ�(��ʊO)
+        //UIを初期位置に設定
         transform.position = firstPosition;
-        //�n�܂����u�ԏo�����������̂�
+        //UI表示
         isApear = true;
     }
 
     void Start()
     {
-        fadeObject.GetComponent<Animator>().Play("FadeIn");
+        //フェードインアニメーションを再生
+       // fadeObject.GetComponent<Animator>().Play("FadeOut");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //UI表示中の処理
         if (isApear)
         {
-            //uiAppearFrame -= decelTimeSpeed * Time.deltaTime;//UI�̕\�����Ԃ����炵�Ă���
-            uiAppearSeconds -= decelTimeSpeedNDeltaTime * Time.unscaledDeltaTime;//UI�̕\�����Ԃ����炵�Ă���
-            Vector3 newPosition = transform.position;        //�I�u�W�F�N�g�̍��W����
+            //UIの表示時間を減らす
+            uiAppearSeconds -= decelTimeSpeedNDeltaTime * Time.unscaledDeltaTime;
+
+            //UIを画面中央に表示
+            Vector3 newPosition = transform.position;        
             newPosition = new Vector3(Screen.width * 0.5f,
-                           Screen.height * 0.5f, 0);         //�錾�����ϐ��Ɍ��_����             
-            transform.position = newPosition;                //�V����������ϐ����I�u�W�F�N�g�ɓ���Ȃ���                        
+                           Screen.height * 0.5f, 0);                 
+            transform.position = newPosition;                                     
         }
-        //UI�̕\�����Ԃ�0�ȉ��ɂȂ�����
+        if(uiAppearSeconds <= 2.0f)
+        {
+           //Destroy(fadeObject); //UI削除
+        }
+
+        //表示時間が0以下になったら
         if (uiAppearSeconds <= 0.0f)
         {
-            Time.timeScale = 1;//���Ԓ�~����������
-            isApear = false;     //�o������������
-            Destroy(gameObject); //�I�u�W�F�N�g��j������           
+            Time.timeScale = 1;　//ゲーム再開
+            isApear = false;     //表示終了
+            Destroy(gameObject); //UI削除       
         }
     }
     private void FixedUpdate()
     {
-        //UI�̕\�����Ԃ�0���߂Ȃ�
+        //UI表示注はゲーム停止
         if (uiAppearSeconds > 0.0f)
         {
             //�o�����Ă���Ԃ̓Q�[���̎��Ԃ��~�߂�
